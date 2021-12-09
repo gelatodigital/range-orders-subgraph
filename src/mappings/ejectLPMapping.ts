3ac5dd19327e3f037982e23f823928e03d353a90
Shivva <matthieumariejoseph@gmail.com>
2021-12-09 23:01:17 +0100

    add settlement

--- src/mappings/ejectLPMapping.ts
+++ src/mappings/ejectLPMapping.ts
@@ -1,6 +1,6 @@
 import { log, BigInt } from "@graphprotocol/graph-ts";
 import { RangeOrder } from "../../generated/schema";
-import {EjectLP, LogSetEject, LogEject, LogCancelEject} from "../../generated/EjectLP/EjectLP";
+import {EjectLP, LogSetEject, LogEject, LogCancelEject, LogSettle} from "../../generated/EjectLP/EjectLP";
 
 export function handleSetEjectLP(event: LogSetEject): void {
   let entity = RangeOrder.load(event.params.tokenId.toString());
@@ -23,9 +23,6 @@ export function handleSetEjectLP(event: LogSetEject): void {
 
   entity.tickThreshold = BigInt.fromI32(event.params.orderParams.tickThreshold);
   entity.zeroForOne = event.params.orderParams.ejectAbove;
-  entity.ejectDust = event.params.orderParams.ejectDust;
-  entity.amount0Min = event.params.orderParams.amount0Min;
-  entity.amount1Min = event.params.orderParams.amount1Min;
   entity.receiver = event.params.orderParams.receiver.toHexString();
   entity.feeToken = event.params.orderParams.feeToken.toHexString();
   entity.resolver = event.params.orderParams.resolver.toHexString();
@@ -74,6 +71,29 @@ export function handleCancel(
   entity.updatedAt = event.block.timestamp;
   entity.updatedAtBlock = event.block.number;
   entity.updatedAtBlockHash = event.block.hash;
+
+  entity.save();
+}
+
+export function handleSettlement(
+  event: LogSettle
+): void {
+  let entity = RangeOrder.load(event.params.tokenId.toString());
+
+  if (entity == null) {
+    log.debug("LP eject don't exist {}", [event.params.tokenId.toString()]);
+    return;
+  }
+
+  entity.status = "expired";
+  entity.executedTxHash = event.transaction.hash;
+  entity.updatedAt = event.block.timestamp;
+  entity.updatedAtBlock = event.block.number;
+  entity.updatedAtBlockHash = event.block.hash;
+
+  entity.amount0 = event.params.amount0Out;
+  entity.amount1 = event.params.amount1Out;
+  entity.feeAmount = event.params.feeAmount;
 
   entity.save();
 }
